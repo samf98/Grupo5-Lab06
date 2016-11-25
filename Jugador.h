4a5cf40f2863eee604d6954f1a3e220a5ea7f9a2
Samir Facussé <samir.facusse@unitec.edu>
2016-11-25 13:47:46 -0600

    hi

---
+++ Jugador.h
@@ -0,0 +1,13 @@
+#include "Persona.h"
+
+class Jugador: Persona
+{
+	private:
+		string posicion;
+	public:
+		Jugador(string,string,string,int,string);
+		~Jugador();
+		void setPosicion(string);
+		string getPosicion();
+		void toString();
+}
